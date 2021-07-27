0e8c17c962a9c505c1b98473fd092345ebda22b3
Mauricio Medvescig <mauricio.m@geopagos.com>
2021-07-27 10:45:52 -0300

    Added ping

--- src/SftpAdapter.php
+++ src/SftpAdapter.php
@@ -623,9 +623,7 @@ class SftpAdapter extends AbstractFtpAdapter
      */
     public function isConnected()
     {
-        $this->connection->ping();
-
-        if ($this->connection instanceof SFTP && $this->connection->isConnected()) {
+        if ($this->connection instanceof SFTP && $this->connection->ping()) {
             return true;
         }
 
